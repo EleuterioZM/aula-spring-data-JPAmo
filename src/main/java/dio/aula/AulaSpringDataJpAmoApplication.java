59d2b7a322f186baee78598096b8d71b4fce0931
Eleuterio Zacarias Mabecuane <122021325+EleuterioZM@users.noreply.github.com>
2025-02-19 20:20:07 +0200

    #Featute: Spring Boot JPA

---
+++ src/main/java/dio/aula/AulaSpringDataJpAmoApplication.java
@@ -0,0 +1,13 @@
+package dio.aula;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AulaSpringDataJpAmoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AulaSpringDataJpAmoApplication.class, args);
+	}
+
+}
---
+++ src/main/java/dio/aula/StarApp.java
@@ -0,0 +1,26 @@
+package dio.aula;
+
+import dio.aula.model.User;
+import dio.aula.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class StarApp implements CommandLineRunner {
+   @Autowired
+    private  UserRepository userRepository;
+    @Override
+    public void run(String... args) throws Exception {
+ User user = new User();
+
+ user.setName("Eleuterio Zacarias");
+ user.setUsername("Eleuterio Zacarias");
+ user.setPassword("Eleuterio");
+ userRepository.save(user);
+
+ for (User u : userRepository.findAll()) {
+     System.out.println(u);
+ }
+    }
+}
