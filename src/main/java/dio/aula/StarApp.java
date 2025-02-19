1802a1658bf4b57ccaa41f7b1febd7c31db2ef93
Eleuterio Zacarias Mabecuane <122021325+EleuterioZM@users.noreply.github.com>
2025-02-19 20:59:33 +0200

    #Improvment: JPQL

--- src/main/java/dio/aula/StarApp.java
+++ src/main/java/dio/aula/StarApp.java
@@ -6,21 +6,36 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+
 @Component
 public class StarApp implements CommandLineRunner {
-   @Autowired
-    private  UserRepository userRepository;
+    @Autowired
+    private UserRepository userRepository;
+
     @Override
     public void run(String... args) throws Exception {
- User user = new User();
+        // Buscar usuários por nome
+        List<User> users = userRepository.findByName("Eleuterio");
+        for (User user : users) {
+            System.out.println(user);
+        }
 
- user.setName("Eleuterio Zacarias");
- user.setUsername("Eleuterio Zacarias");
- user.setPassword("Eleuterio");
- userRepository.save(user);
-
- for (User u : userRepository.findAll()) {
-     System.out.println(u);
- }
+        // Inserir um novo usuário
+        insertUser();
     }
-}
+
+
+    private void insertUser() {
+            User user = new User();
+
+            user.setName("Zacarias ");
+            user.setUsername("Zacarias ");
+            user.setPassword("Eleuterio");
+            userRepository.save(user);
+
+            for (User u : userRepository.findAll()) {
+                System.out.println(u);
+            }
+        }
+    }
--- src/main/java/dio/aula/repository/UserRepository.java
+++ src/main/java/dio/aula/repository/UserRepository.java
@@ -2,6 +2,14 @@ package dio.aula.repository;
 
 import dio.aula.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
+
+    @Query("SELECT u from User u where u.name like %:name%")
+    List<User> findByName(@Param("name") String name);
+
 }
